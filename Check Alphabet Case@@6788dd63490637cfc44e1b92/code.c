8e8f92055dfdd4f1b463de11ce8eb53f8b161139
mahesh4596 <maheshsingla2007@gmail.com>
2025-01-20 20:57:58 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -3,9 +3,10 @@
 int main() {
     char c;
     scanf("%c",&c);
-    if(c==A || c==B || c==C || c==D || c==E || c==F || c==G || c==H || c==I || 
-    c==J || c==J || c==K || c==L || c==M || c==N || c==O || c==P || c==Q || 
-    c==R || c==S || c==T || c==U || c==V || c==W || c==X || c==Y || c==Z)
+    if(c=="A" || c=="B" || c=="C" || c=="D" || c=="E" || c=="F" || c=="G" || 
+    c=="H" || c=="I" || c=="J" || c=="J" || c=="K" || c=="L" || c=="M" || 
+    c=="N" || c=="O" || c=="P" || c=="Q" || c=="R" || c=="S" || c=="T" || 
+    c=="U" || c=="V" || c=="W" || c=="X" || c=="Y" || c=="Z")
         printf("Uppercase");
     else
         printf("Lowercase");
