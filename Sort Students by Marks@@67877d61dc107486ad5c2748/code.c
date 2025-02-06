00ebf1da1250a806f32f89818a64b3d346cfca9a
Mahesh Singla <189551428+mahesh4596@users.noreply.github.com>
2025-02-06 17:22:28 +0530

    Solution to Sort Students by Marks

--- Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -29,7 +29,7 @@ int main() {
     for(int i=0;i<=n;i++)
     {
         printf("Roll Number: %d, Name: %s, Marks: %.2f",students[i].rollno,students[i].name,students[i].marks);
-        printf("/n");
+        printf("\n");
     }
 
 }
