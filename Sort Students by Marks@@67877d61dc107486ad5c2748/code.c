cff86d17a45626410f69880def9b48b0e24807c8
Mahesh Singla <189551428+mahesh4596@users.noreply.github.com>
2025-02-06 17:18:24 +0530

    Solution to Sort Students by Marks

---
+++ Sort Students by Marks@@67877d61dc107486ad5c2748/code.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct STUDENT {
+    int rollno;
+    char name[100];
+    float marks;
+};
+
+int main() {
+    int n;
+    float temp;
+    scanf("%d", &n);
+
+    struct STUDENT students[n];
+    for (int i=0;i<=n;i++) {
+        scanf("%d %s %f", &students[i].rollno, students[i].name, &students[i].marks);
+    }
+    for(int r=0;r<=n;r++)
+    {
+        for(int c=0;c<=n-1-r;c++)
+        {
+            if(students[c].marks>students[c+1].marks)
+            {
+                temp=students[c].marks;
+                students[c].marks=students[c+1].marks;
+                students[c+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<=n;i++)
+    {
+        printf("Roll Number: %d, Name: %s, Marks: %.2f",students[i].rollno,students[i].name,students[i].marks);
+    }
+
+}
