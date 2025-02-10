e43471afc06c4a5891652de8db0b7ba01d3c397b
Mahesh Singla <189551428+mahesh4596@users.noreply.github.com>
2025-02-10 19:06:12 +0530

    Solution to Manage Book Library

---
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct Book
+{
+    int price;
+    char author[100],title[100];
+};
+
+int main() {
+    int n;
+    scanf("%d",&n);
+
+    struct Book books[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%s %s %d",&books[i].title,&books[i].author,&books[i].price);
+    }
+    int p;
+    scanf("%d",&p);
+    for(int i=1;i<n;i++)
+    {
+        if(books[i].price>books[i+1].price)
+            printf("Books above price %.2f: Title: %s, Author: %s, Price: %.2f\n",p,books[i].title,books[i].author,books[i].price);       
+    }
+
+}
