fca9518f275728e618fc25b14d9d07586daa96b7
Mahesh Singla <189551428+mahesh4596@users.noreply.github.com>
2025-02-10 19:11:41 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -15,16 +15,16 @@ int main() {
         scanf("%s %s %d", books[i].title, books[i].author, &books[i].price);
     }
 
-    int p;
-    scanf("%d", &p);
+    float p;
+    scanf("%f", &p);
 
     int found = 0;
     for (int i = 0; i < n; i++) 
     { 
         if (books[i].price > p) 
         {  
-            printf("Books above price %.2f\n",p);
-            printf("Title: %s, Author: %s, Price: %d\n", books[i].title, books[i].author, books[i].price);
+            printf("Books above price %.2f:\n",p);
+            printf("Title: %s, Author: %s, Price: %.2f\n", books[i].title, books[i].author, books[i].price);
             found = 1;
         }
     }
