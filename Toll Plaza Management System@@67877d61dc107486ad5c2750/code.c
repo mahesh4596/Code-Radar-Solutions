44356fa52b27171c91681b6c6297ee280a4dccb4
Mahesh Singla <maheshsingla2007@gmail.com>
2025-02-13 16:56:04 +0530

    Solution to Toll Plaza Management System

--- Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 struct Vehicle
 {
-    char name[100],vno[100];
+    char name[100], vno[100];
     float tamount;
 };
 
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
     struct Vehicle vehicle[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%s %s %f",&vehicle[i].vno,&vehicle[i].name,&vehicle[i].tamount);
+        scanf("%s %s %f", vehicle[i].vno, vehicle[i].name, &vehicle[i].tamount);
     }
-    float tcar = 0,ttruck = 0,tcar = 0;
-    for(int i=0;i<n;i++)
+    float tcar = 0, ttruck = 0, tbike = 0;
+    for (int i = 0; i < n; i++)
     {
-        if(vehicle[i].name == "Car")
+        if (strcmp(vehicle[i].name, "Car") == 0)
             tcar += vehicle[i].tamount;
-        else if(vehicle[i].name == "Truck")
+        else if (strcmp(vehicle[i].name, "Truck") == 0)
             ttruck += vehicle[i].tamount;
         else
             tbike += vehicle[i].tamount;
     }
-    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",tcar,ttruck,tbike);
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f\n", tcar, ttruck, tbike);
+
 }
