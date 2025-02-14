9cedb2b64138b0d762ace05c20393c6f38d43285
Mahesh Singla <maheshsingla2007@gmail.com>
2025-02-14 20:26:42 +0530

    Solution to Find the Frequency of Each Character

--- Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
+++ Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <string.h>
-int countSubstringOccurrences(char str[]) 
+
+int countSubstringOccurrences(char str[], char sub[]) 
 {
     int count = 0;
     char *pos = str;
-    while ((pos = strstr(pos)) != NULL) 
+    
+    while ((pos = strstr(pos, sub)) != NULL) 
     {
         count++;
-        pos++;
+        pos++;  
     }
     return count;
 }
+
 int main() {
-    char str[100];
-    int occurrences = countSubstringOccurrences(str);
+    char str[100], sub[50];
+
+    fgets(str, sizeof(str), stdin);
+    fgets(sub, sizeof(sub), stdin);
+
+    str[strcspn(str, "\n")] = '\0';  
+    sub[strcspn(sub, "\n")] = '\0';  
+
+    int occurrences = countSubstringOccurrences(str, sub);
+
     printf("%s: %d\n", sub, occurrences);
+
+    return 0;
 }
