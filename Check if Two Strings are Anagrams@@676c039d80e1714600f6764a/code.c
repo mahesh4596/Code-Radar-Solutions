c498fcc3453d1047e3e378b4213b7f05cea6b6f4
Mahesh Singla <maheshsingla2007@gmail.com>
2025-02-14 20:19:20 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -2,25 +2,44 @@
 
 int main()
 {
-    char s1[100],s2[100];
-    fgets(s1, sizeof(s), stdin);
-    fgets(s2, sizeof(s), stdin);
-    int as1[256]={0},as2[256]={0};
-    int len;
-    for(int len = 0; s1[len]!='\o';len++);
-    for(int i=0;i<len;i++)
+    char s1[100], s2[100];
+    
+    fgets(s1, sizeof(s1), stdin);
+    fgets(s2, sizeof(s2), stdin);
+
+    int as1[256] = {0}, as2[256] = {0};
+
+    int len = 0;
+    while (s1[len] != '\0' && s1[len] != '\n') 
     {
-        int asciiIndex=s1[i];
-        as1[asciiIndex]++;
-        asciiIndex=s2[i];
-        as2[asciiIndex]++;
+        len++;
     }
-    for(int i=0;i<256;i++)
+
+    int len2 = 0;
+    while (s2[len2] != '\0' && s2[len2] != '\n') 
     {
-        if(as1[i]!=as2[i])
+        len2++;
+    }
+
+    if (len != len2) 
+    {
+        printf("No\n");
+        return 0;
+    }
+
+    for (int i = 0; i < len; i++) 
+    {
+        as1[(unsigned char)s1[i]]++;
+        as2[(unsigned char)s2[i]]++;
+    }
+    
+    for (int i = 0; i < 256; i++) 
+    {
+        if (as1[i] != as2[i]) 
         {
-            printf("No");
+            printf("No\n");
+            return 0;
         }
     }
-    printf("Yes");
+    printf("Yes\n");
 }
