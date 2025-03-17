f4ef71bceb3a41db3448e68499caa64766a39e7e
Mahesh Singla <maheshsingla2007@gmail.com>
2025-03-17 20:52:39 +0530

    Solution to Count Elements Greater Than a Given Value

---
+++ Count Elements Greater Than a Given Value@@676c02002dbc8c3e7a280b8a/code.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void getFrequence(int ary[], int n) 
+{
+    int counted[n]; 
+    for (int i = 0; i < n; i++) 
+    {
+        counted[i] = 0;
+    }
+
+    int maxFreq = 0, maxElement = ary[0];
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (counted[i]) 
+            continue;
+
+        int count = 0;
+        if (ary[i] > a) 
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
+
+int main() 
+{
+    int n,a;
+    scanf("%d %d", &n, &a);
+
+    int ary[n];
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &ary[i]);
+    }
+
+    getFrequence(ary, n, a);
+    return 0;
+}
