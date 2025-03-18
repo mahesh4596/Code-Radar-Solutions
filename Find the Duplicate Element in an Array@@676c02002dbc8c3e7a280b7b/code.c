8c08461a2d94c8072251537268830ce32fb84113
Mahesh Singla <maheshsingla2007@gmail.com>
2025-03-18 18:40:03 +0530

    Solution to Find the Duplicate Element in an Array

---
+++ Find the Duplicate Element in an Array@@676c02002dbc8c3e7a280b7b/code.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void getFrequence(int ary[], int n) 
+{
+    int counted[n]; 
+    for (int i = 0; i < n; i++) 
+    {
+        counted[i] = 0;
+    }
+
+    int maxFreq = 0, maxElement = ary[0];
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (counted[i]) 
+            continue;
+
+        int count = 1;
+        for (int j = i + 1; j < n; j++) 
+        {
+            if (ary[i] == ary[j]) 
+            {
+                count++;
+                counted[j] = 1; 
+            }
+        }
+        if (count > maxFreq) 
+        {
+            maxFreq = count;
+            maxElement = ary[i];
+        }
+    }
+    printf("%d", maxElement);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int ary[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &ary[i]);
+    }
+
+    getFrequence(ary, n);
+    return 0;
+}
