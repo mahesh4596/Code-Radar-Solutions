27cd0fdb0566708892d393f0b4091e8f5207e200
Mahesh Singla <maheshsingla2007@gmail.com>
2025-04-07 20:06:50 +0530

    Solution to Count the Occurrences of a Character

---
+++ Count the Occurrences of a Character@@676c039d80e1714600f6764b/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char str[100],s[100];
+    int v=0;
+    fgets(str,sizeof(str),stdin);
+    fgets(s,sizeof(s),stdin);
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i] == 's')
+            v++;
+    }
+    printf("%d",v);
+}
