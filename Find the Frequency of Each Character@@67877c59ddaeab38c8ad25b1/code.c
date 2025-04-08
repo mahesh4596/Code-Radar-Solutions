d52f3c9dfdc2f4ed4d2b18efa91df6d0d168b499
Mahesh Singla <maheshsingla2007@gmail.com>
2025-04-08 19:22:51 +0530

    Solution to Find the Frequency of Each Character

--- Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
+++ Find the Frequency of Each Character@@67877c59ddaeab38c8ad25b1/code.c
@@ -6,13 +6,23 @@ int main()
     char str[100];
     fgets(str, sizeof(str), stdin);
 
+    int counted[n] ; 
+    for (int i = 0; i < n; i++)
+        counted[i] = 0;
+
     for(int i = 0; str[i] != '\0'; i++)
     {
-        int count = 0;
+        if (counted[i]) 
+            continue;
+
+        int count = 1;
         for(int j = 0; str[j] != '\0'; j++)
         {
             if(str[i] == str[j])
+            {
                 count++;
+                counted[j] = 1;
+            }
         }
         printf("%c: %d\n",str[i],count);
     }
